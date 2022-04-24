5e4e26d817d7892e271cb1669d3aa5570151e3a2
Bao <baohua01234@gmail.com>
2022-04-24 10:05:12 +0700

    Update syntax and function names, add drag to column that is empy

--- src/components/molecules/DragNDrop/DragNDrop.jsx
+++ src/components/molecules/DragNDrop/DragNDrop.jsx
@@ -7,26 +7,15 @@ function DragNDrop(props) {
     const [noNeedDragging, setNoNeedDragging] = useState(false);
     const [noNeedList, setNoNeedList] = useState([])
 
+    //use for drag and drop of pages
     const dragItem = useRef();
     const dragItemNode = useRef();
     const noNeedItem = useRef();
 
+    //use for drag and drop of no need panal
     const rootDragNoNeedItem = useRef();
     const dragNoNeedItem = useRef();
     const dragNoNeedItemNode = useRef();
-
-    useEffect(() => {
-        setList(data);
-    }, [setList, data])
-
-    useEffect(() => {
-        if(!dragging && dragItemNode.current !== null){
-            console.log('---------start re-order---------')
-            reOrderPages()
-            dragItem.current = null;
-            dragItemNode.current = null;
-        }
-    }, [list, dragging])
     
     const handletDragStart = (e, item) => {
         console.log('Starting to drag', item)
@@ -39,16 +28,17 @@ function DragNDrop(props) {
             setDragging(true); 
         },0)       
     }
+
     const handleDragEnter = (e, targetItem) => {
         console.log('Entering a drag target', targetItem)
 
         const currentDragItem = list[targetItem.pageIndex].columns[targetItem.columnIndex].child[dragItem.current.childIndex]
         console.log('drag enter current drag item:', currentDragItem)
-        const {status, index} = checkForExists(currentDragItem)
+        const {status, noNeedIndex} = checkForExists(currentDragItem)
         //if we drag outside the no need panel, we remove the item is stored in list
         if(status){
             noNeedItem.current = null
-            noNeedList.splice(index, 1);
+            noNeedList.splice(noNeedIndex, 1);
             setNoNeedList([...noNeedList])
         }
         else{
@@ -65,6 +55,7 @@ function DragNDrop(props) {
             }
         }
     }
+
     const handleDragEnd = (e) => {
         console.log('drag end')
         
@@ -81,6 +72,7 @@ function DragNDrop(props) {
         setDragging(false);
         dragItemNode.current.removeEventListener('dragend', handleDragEnd)
     }
+
     const getStyles = (item) => {
         if(dragItem.current && item){
             if (dragItem.current.pageIndex === item.pageIndex && dragItem.current.columnIndex === item.columnIndex && dragItem.current.childIndex === item.childIndex) {
@@ -100,77 +92,23 @@ function DragNDrop(props) {
         return "dnd-item-wrapper"
     }
 
-    const reOrderPages = () => {
-        setIsReOrder(true)
-        setPages([...list])
-    }
-
-    const onNoNeedDragEnter = (e) => {
-        const currentDragItem = list[dragItem.current.pageIndex].columns[dragItem.current.columnIndex].child[dragItem.current.childIndex];
-        noNeedItem.current = currentDragItem
-        console.log('??????current drag item from is:', currentDragItem)
-        const {status} = checkForExists(currentDragItem);
-        if(!status){
-            noNeedList.splice(0, 0, currentDragItem)
-            setNoNeedList([...noNeedList])
-        }
-    }
-
-    const checkForExists = (currentDragItem) => {
-        for(const [pageIndex, page] of data.entries()){
-            for(const [columnIndex, column] of page.columns.entries()){
-                for(const [childIndex, child] of column.child.entries()){
-                    if(currentDragItem){
-                        if(child.id === currentDragItem.id){
-                            for(const [noNeedIndex, noNeedItem] of noNeedList.entries()){
-                                if(noNeedItem.id === currentDragItem.id){
-                                    return {index: noNeedIndex, status: true}
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return {status: false}
-    }
-
-    const handleItemDragStart = (e, item) => {
-        console.log('Starting to drag item 1', item)
-
-        dragNoNeedItemNode.current = e.target;
-        dragNoNeedItemNode.current.addEventListener('dragend', handleItemDragEnd)
-        dragNoNeedItem.current = item;
-        rootDragNoNeedItem.current = item;
-
-        setTimeout(() => {
-            setNoNeedDragging(true); 
-        },0)     
-    }
-
-    const handleItemDragEnd = (e) => {
-        console.log('drag item 1 end')
-        setNoNeedDragging(false);
-        dragNoNeedItemNode.current.removeEventListener('dragend', handleItemDragEnd)
-    }
-
     const handleColumnDragEnter = (e, targetItem) => {
         console.log('no need drag enter')
         console.log(targetItem)
 
         if (dragNoNeedItemNode.current !== e.target) {
             console.log('Target is NOT the same as no need dragged item')
-            const {index} = rootDragNoNeedItem.current
-            const currentNoNeedDragging = noNeedList[index]
+            const {noNeedIndex} = rootDragNoNeedItem.current
+            const currentNoNeedDragging = noNeedList[noNeedIndex]
             if(currentNoNeedDragging){
-                const {status, pageIndex, columnIndex, childIndex} = checkForNoNeedExists(currentNoNeedDragging)
-                //check if list already has data in no need list, if not we append it to list
+                const {status, pageIndex, columnIndex, childIndex} = checkForExists(currentNoNeedDragging)
+                //check if list already has 'current no need item drag' in no need list, if not we append it to list
                 if(!status){
                     list[targetItem.pageIndex].columns[targetItem.columnIndex].child.splice(targetItem.childIndex, 0, currentNoNeedDragging)
                     dragNoNeedItem.current = targetItem
                     setList([...list])
                 }
-                else//we we found no need item in list, we delete the old item to prevent duplicate when we add the new one
+                else//if we found no need item in list, we delete the old item to prevent duplicate when we add a new one
                 {
                     list[pageIndex].columns[columnIndex].child.splice(childIndex, 1)
                     list[targetItem.pageIndex].columns[targetItem.columnIndex].child.splice(targetItem.childIndex, 0, currentNoNeedDragging)
@@ -181,15 +119,45 @@ function DragNDrop(props) {
         }
     }
 
-    const checkForNoNeedExists = (currentDragItem) => {
-        for(const [pageIndex, page] of data.entries()){
+    const handleNoNeedItemDragStart = (e, item) => {
+        console.log('Starting to drag no need item', item)
+
+        dragNoNeedItemNode.current = e.target;
+        dragNoNeedItemNode.current.addEventListener('dragend', handleNoNeedItemDragEnd)
+        dragNoNeedItem.current = item;
+        rootDragNoNeedItem.current = item;
+
+        setTimeout(() => {
+            setNoNeedDragging(true); 
+        },0)     
+    }
+
+    const handleNoNeedItemDragEnter = (e) => {
+        const currentDragItem = list[dragItem.current.pageIndex].columns[dragItem.current.columnIndex].child[dragItem.current.childIndex];
+        noNeedItem.current = currentDragItem
+        console.log('??????current drag no need item from is:', currentDragItem)
+        const {status} = checkForExists(currentDragItem);
+        if(!status){
+            noNeedList.splice(0, 0, currentDragItem)
+            setNoNeedList([...noNeedList])
+        }
+    }
+
+    const handleNoNeedItemDragEnd = (e) => {
+        console.log('drag no need end')
+        setNoNeedDragging(false);
+        dragNoNeedItemNode.current.removeEventListener('dragend', handleNoNeedItemDragEnd)
+    }
+
+    const checkForExists = (currentDragItem) => {
+        for(const [pageIndex, page] of list.entries()){
             for(const [columnIndex, column] of page.columns.entries()){
                 for(const [childIndex, child] of column.child.entries()){
                     if(currentDragItem){
                         if(child.id === currentDragItem.id){
                             for(const [noNeedIndex, noNeedItem] of noNeedList.entries()){
                                 if(noNeedItem.id === currentDragItem.id){
-                                    return {pageIndex: pageIndex, columnIndex: columnIndex, childIndex: childIndex, status: true}
+                                    return {noNeedIndex: noNeedIndex, pageIndex: pageIndex, columnIndex: columnIndex, childIndex: childIndex, status: true}
                                 }
                             }
                         }
@@ -199,15 +167,34 @@ function DragNDrop(props) {
         }
         return {status: false}
     }
-    
+
+    const reOrderPages = () => {
+        setIsReOrder(true)
+        setPages([...list])
+    }
+
+    useEffect(() => {
+        setList(data);
+    }, [setList, data])
+
+    useEffect(() => {
+        if(!dragging && dragItemNode.current !== null){
+            console.log('---------start re-order---------')
+            reOrderPages()
+            dragItem.current = null;
+            dragItemNode.current = null;
+        }
+    }, [list, dragging])
+
     useEffect(() => {
         if(!noNeedDragging && dragNoNeedItemNode.current !== null){
             console.log('---------start re-order when no need---------')
             reOrderPages()
             
+            //after we drag end, we remove 'current no need item drag' from no need list
             if(rootDragNoNeedItem.current){
-                const {index} = rootDragNoNeedItem.current
-                noNeedList.splice(index, 1)
+                const {noNeedIndex} = rootDragNoNeedItem.current
+                noNeedList.splice(noNeedIndex, 1)
                 setNoNeedList([...noNeedList])
             }
 
@@ -226,6 +213,7 @@ function DragNDrop(props) {
                             page.columns.map((column, columnIndex) => (
                                 <div key={columnIndex} 
                                     className='dnd-group'
+                                    onDragEnter={dragging && !column.child.length ? (e) => handleDragEnter(e, {pageIndex, columnIndex, childIndex: 0}): null}
                                 >
                                     <div className='group-title'>Page: {pageIndex}, Column: {columnIndex}</div>
                                     <div key={columnIndex} className='dnd-group-1'>
@@ -233,13 +221,13 @@ function DragNDrop(props) {
                                             <div 
                                                 key={child.id} 
                                                 onDragEnter={noNeedDragging ? (e) => handleColumnDragEnter(e, {pageIndex, columnIndex, childIndex}): null}
-                                                className={noNeedDragging?getNoNeedStyles({pageIndex, columnIndex, childIndex}):"dnd-item-wrapper"}
+                                                className={noNeedDragging ? getNoNeedStyles({pageIndex, columnIndex, childIndex}): "dnd-item-wrapper"}
                                             >
                                                 <div 
                                                     draggable 
                                                     onDragStart={(e) => handletDragStart(e, {pageIndex, columnIndex, childIndex})} 
-                                                    onDragEnter={dragging?(e) => {handleDragEnter(e, {pageIndex, columnIndex, childIndex})}:null} 
-                                                    className={dragging?getStyles({pageIndex, columnIndex, childIndex}):"dnd-item"}
+                                                    onDragEnter={dragging ? (e) => {handleDragEnter(e, {pageIndex, columnIndex, childIndex})}: null} 
+                                                    className={dragging ? getStyles({pageIndex, columnIndex, childIndex}): "dnd-item"}
                                                 >
                                                     {child.header}
                                                 </div>
@@ -256,14 +244,14 @@ function DragNDrop(props) {
                         <div>Drag and drop to Add or Remove sections.</div>
                         <div 
                             className='drag-no-need'
-                            onDragEnter={dragging ? (e) => onNoNeedDragEnter(e): null}
+                            onDragEnter={dragging ? (e) => handleNoNeedItemDragEnter(e): null}
                         >
-                            {noNeedList && noNeedList.map((noNeedItem, index) => (
+                            {noNeedList && noNeedList.map((noNeedItem, noNeedIndex) => (
                                 <div 
                                     draggable
                                     className='drag-no-need-item' 
-                                    key={noNeedItem.header + index}
-                                    onDragStart={(e) => handleItemDragStart(e, {index})}
+                                    key={noNeedItem.header + noNeedIndex}
+                                    onDragStart={(e) => handleNoNeedItemDragStart(e, {noNeedIndex})}
                                 >
                                     {noNeedItem.header}
                                 </div>
